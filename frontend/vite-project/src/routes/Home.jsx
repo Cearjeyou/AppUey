fa24da74b38bf9649a6015d31bae23968aa19aba
Carlos Arturo Bermudez Rios <87234948+Cearjeyou@users.noreply.github.com>
2023-04-29 13:21:43 -0500

    Update Home.jsx
    Actualización rutas

--- frontend/vite-project/src/routes/Home.jsx
+++ frontend/vite-project/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
